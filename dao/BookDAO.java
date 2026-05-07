a9d50942438abb0a561b4805eae6dde7fc5f439a
aya alharazin <alharazinaya@gmail.com>
2026-05-07 12:49:36 +0300

    Refactor getAllbooksids method in BookDAO to simplify entity manager usage and remove unnecessary error handling, enhancing code clarity.

--- dao/BookDAO.java
+++ dao/BookDAO.java
@@ -4,11 +4,10 @@
  */
 package dao;
 
+
 import config.JPAUtil;
-import java.util.Collections;
 import java.util.List;
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 
 
 
@@ -17,23 +16,16 @@ import javax.persistence.TypedQuery;
  * @author aya
  */
 public class BookDAO {
+    
     public List<Integer> getAllbooksids() {
-        EntityManager em = null;
-        try {
-            em = JPAUtil.getEntityManager();
-            TypedQuery<Integer> q = em.createQuery(
-                    "select b.bookId from Book b",
-                    Integer.class
-            );
-            return q.getResultList();
-        } catch (Exception ex) {
-            System.getLogger(BookDAO.class.getName())
-                    .log(System.Logger.Level.ERROR, (String) null, ex);
-            return Collections.emptyList();
-        } finally {
-            if (em != null) {
-                em.close();
-            }
+        EntityManager em=null;
+        try{
+            em =JPAUtil.getEntityManager();
+            return em.createQuery("select b.bookId from Book b")
+                .getResultList();
+        }finally{
+            em.close();
         }
+        
     }
 }
